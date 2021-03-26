815c384c5d1b1f862517d5cd07296feba3eb3152
MohammadReza Iranmanesh <imohammadreza.ir@gmail.com>
2021-03-27 01:56:45 +0430

    fix: react-spring animation on production
    see: https://github.com/pmndrs/react-spring/issues/1078#issuecomment-791637097

---
+++ postinstall.js
@@ -0,0 +1,15 @@
+const replace = require('replace-in-file');
+
+const removeAllSideEffectsFalseFromReactSpringPackages = async () => {
+  try {
+    await replace({
+      files: 'node_modules/@react-spring/*/package.json',
+      from: '"sideEffects": false',
+      to: '"sideEffects": true',
+    });
+  } catch (e) {
+    console.log('error while trying to remove string "sideEffects:false" from react-spring packages', e);
+  }
+};
+
+removeAllSideEffectsFalseFromReactSpringPackages();
