1835b6322783dcdfbe5a93d7756fd075b765575a
MohammadReza Iranmanesh <imohammadreza.ir@gmail.com>
2021-03-27 02:36:51 +0430

    feat: add new favicons

--- components/MetaTags.jsx
+++ components/MetaTags.jsx
@@ -7,26 +7,28 @@ function MetaTags() {
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <title>MohammadReza Iranmanesh</title>
       <meta name="description" content="MohammadReza Iranmanesh Personal Website | Frontend Enginner" />
+      <meta name="keywords" content="MohammadReza, Iranmanesh, portfolio, resume, front-end, frontend, engineer, react" />
+      <meta name="robots" content="index, follow" />
       <meta name="og:title" property="og:title" content="MohammadReza Iranmanesh" />
       <meta name="og:description" property="og:title" content="MohammadReza Iranmanesh Personal Website | Frontend Enginner" />
-      <meta property="og:image" content="https://imohammadreza.ir/favicon/android-chrome-192x192.png" />
+      <meta property="og:image" content="https://imohammadreza.ir/favicon/apple-touch-icon.png" />
       <meta property="og:url" content="https://imohammadreza.ir" />
       <meta property="og:type" content="website" />
       <meta name="twitter:title" content="MohammadReza Iranmanesh" />
       <meta name="twitter:site:id" content="@iMohammadReza_" />
       <meta name="twitter:creator" content="@iMohammadReza_" />
       <meta name="twitter:description" content="MohammadReza Iranmanesh Personal Website | Frontend Enginner" />
-      <meta name="twitter:image" content="https://imohammadreza.ir/favicon/android-chrome-192x192.png" />
+      <meta name="twitter:image" content="https://imohammadreza.ir/favicon/apple-touch-icon.png" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="author" content="MohammadReza Iranmanesh" />
       <meta name="theme-color" content="#000000" />
-      <meta name="keywords" content="MohammadReza, Iranmanesh, portfolio, resume, front-end, frontend, engineer, react" />
-      <meta name="robots" content="index, follow" />
+      <meta name="msapplication-TileColor" content="#da532c" />
       <link rel="icon" href="favicon/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
       <link rel="manifest" href="/manifest.json" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
     </Head>
   );
 }
