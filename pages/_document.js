22b2cd6d833c058e611bb4faf52b775b1c9d1fef
MohammadReza Iranmanesh <imohammadreza.ir@gmail.com>
2021-06-15 23:23:09 +0430

    fix: use nextjs 11 font optimization

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html dir="ltr" lang="en">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Roboto+Slab:wght@300;700&family=Almarai:wght@300;800&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
